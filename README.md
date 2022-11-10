94939d1b4a7a24aaa83d2083fed6226a34f9f9a5
Kevin Lieberman <kevin.lieberman@harley-davidson.com>
2022-11-10 09:04:02 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# VentControlApp
---
+++ app/src/main/java/com/lieberland/ventcontrol/MainActivity.java
@@ -0,0 +1,203 @@
+package com.lieberland.ventcontrol;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.lieberland.ventcontrol.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    MqttAndroidClient client;
+
+    TextView debugText;
+    Button connectButton;
+    Button oneButton;
+    Button twoButton;
+    Button threeButton;
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(this);
+
+        oneButton = findViewById(R.id.oneButton);
+        oneButton.setOnClickListener(this);
+
+        twoButton = findViewById(R.id.twoButton);
+        twoButton.setOnClickListener(this);
+
+        threeButton = findViewById(R.id.threeButton);
+        threeButton.setOnClickListener(this);
+
+        debugText = findViewById(R.id.debugText);
+
+        String clientId = MqttClient.generateClientId();
+        //client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.mqttdashboard.com:1883",clientId);
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://hummbug2:1883",clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this,"connected!!",Toast.LENGTH_LONG).show();
+                    setSubscription();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this,"connection failed!!",Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                debugText.setText(new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.d(TAG, "delivery complete");
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setSubscription(){
+        try{
+            client.subscribe("voltages",0);
+        }catch (MqttException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void conn(View v){
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this,"connected!!",Toast.LENGTH_LONG).show();
+                    setSubscription();
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this,"connection failed!!",Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void disconn(View v){
+
+        try {
+            IMqttToken token = client.disconnect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this,"Disconnected!!",Toast.LENGTH_LONG).show();
+
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this,"Could not diconnect!!",Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.connectButton) {
+            Log.d(TAG, "Connect button pressed");
+        }
+
+        if(view.getId() == R.id.oneButton) {
+            Log.d(TAG, "One button pressed");
+        }
+
+        if(view.getId() == R.id.twoButton) {
+            Log.d(TAG, "Two button pressed");
+        }
+
+        if(view.getId() == R.id.threeButton) {
+            Log.d(TAG, "Three button pressed");
+        }
+    }
+}
